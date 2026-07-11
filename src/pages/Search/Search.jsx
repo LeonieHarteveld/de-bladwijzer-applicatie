e8e6a32908be17eee67bbe2708133e49dbbac43a
leharteveld <l.e.harteveld@gmail.com>
2026-07-11 17:40:26 +0200

    Opzet voor zoekpagina klaar

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -3,14 +3,14 @@ import SearchBar from '../../components/SearchBar/SearchBar.jsx'
 
 function Search() {
 
-
     return (
         <section
             className={styles.searchpage}>
             <div
-                className={styles.searchcpage__inner}>
+                className={styles.searchpage__inner}>
                 <h1>Zoeken</h1>
                 <SearchBar/>
+                <h2>Of ontdek per genre</h2>
             </div>
         </section>
     )
