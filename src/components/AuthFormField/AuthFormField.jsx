8bb660205b82b402c173e9320a62a5f59adee961
leharteveld <l.e.harteveld@gmail.com>
2026-07-19 22:40:01 +0200

    icons aan AuthFormField toegevoegd

--- src/components/AuthFormField/AuthFormField.jsx
+++ src/components/AuthFormField/AuthFormField.jsx
@@ -1,6 +1,14 @@
 import styles from './AuthFormField.module.scss';
+import emailIcon from "../../assets/icons/email-icon.svg";
+import passwordIcon from "../../assets/icons/password-icon.svg";
 
 function AuthFormField({id, label, type = 'text', name, placeholder, value, onChange, error,}) {
+
+    const icon =
+        type === 'password'
+            ? passwordIcon
+            : emailIcon;
+
     return (
         <div className={styles.formField}>
             <label
@@ -10,16 +18,25 @@ function AuthFormField({id, label, type = 'text', name, placeholder, value, onCh
                 {label}
             </label>
 
-            <input
-                className={styles.formField__input}
-                id={id}
-                type={type}
-                name={name}
-                placeholder={placeholder}
-                value={value}
-                onChange={onChange}
-                error={error}
-            />
+            <div className={styles.formField__inputWrapper}>
+                <img
+                    className={styles.formField__icon}
+                    src={icon}
+                    alt="icon"
+                />
+
+                <input
+                    className={styles.formField__input}
+                    id={id}
+                    type={type}
+                    name={name}
+                    placeholder={placeholder}
+                    value={value}
+                    onChange={onChange}
+                    error={error}
+                />
+            </div>
+
 
             {error && (
                 <p
